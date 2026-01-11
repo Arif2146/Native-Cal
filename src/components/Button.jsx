be2d47510ea1408e6ccc82141ff934e20e58679d
Arif Bepari <arifbepari2006@gmail.com>
2026-01-11 22:02:48 +0530

    10 min

---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, TouchableOpacity} from "react-native";
+import { calColors } from "../colors/colors";
+
+export default function BButton({title,type}) {
+    return(
+        <TouchableOpacity style = {[
+styles.button,
+{
+backgroundColor:
+type === "top"
+? calColors.btnDark
+: type === "right"
+? calColors.btnRight
+: calColors.btnLight,
+},
+]} onPress={()=>{}}>
+            <Text style={{fontSize:34,color: type === "number" ? calColors.black : calColors.btnLight,}}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    button: {
+    height:70,
+    width:70,
+    borderRadius:10,
+    padding:10,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:calColors.btnDark,
+},
+});
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,11 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text,  View } from "react-native";
 import { calColors } from "../colors/colors";
+import BButton from "./Button.jsx";
+
 
 
 export default function Calculator() {
     return (
         <View style={styles.container}>
-            <Text>Calculator</Text>
+            <View style={styles.display}>
+              <Text style={{fontSize:70,fontWeight:'semibold'}}>8765</Text>
+            </View>
+            <View style={styles.Keypad}>
+               <BButton title='C' type='top'/>
+              <BButton title='«' type='top'/>
+<BButton title='%' type='top'/>
+<BButton title='+' type='right'/>
+<BButton title='7' type='number'/>
+<BButton title='8' type='number'/>
+<BButton title='9' type='number'/>
+<BButton title='x' type='right'/>
+<BButton title='6' type='number'/>
+<BButton title='5' type='number'/>
+<BButton title='4' type='number'/>
+<BButton title='-' type='right'/>
+<BButton title='1' type='number'/>
+<BButton title='2' type='number'/>
+<BButton title='3' type='number'/>
+<BButton title='+' type='right'/>
+<BButton title='0' type='number'/>
+<BButton title='00' type='number'/>
+<BButton title='.' type='number'/>
+<BButton title='=' type='right'/>
+            </View>
         </View>
     );
 }
@@ -14,5 +40,22 @@ export default function Calculator() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  display: {
+    flex: 1,
+    backgroundColor: calColors.gray,
+    paddingVertical:20,
+    paddingHorizontal:40,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+  Keypad: {
+    flex: 2,
+    backgroundColor: calColors.light,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap:30,
+    padding:30,
   },
 });
