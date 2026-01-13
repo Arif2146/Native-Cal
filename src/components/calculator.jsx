b365f644ae24b8fed41f9ceb4a4043a68866e551
Arif Bepari <arifbepari2006@gmail.com>
2026-01-13 16:41:25 +0530

    finish

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -17,32 +17,67 @@ const handleNumberInput = (num) => {
     setDisplayValue(displayValue + num);
   }
 }
+const handleOperatorInput = (operator) => {
+  setOperator(operator);
+  setFirstValue(displayValue);
+  setDisplayValue("0");
+}
+const handleEquals = () => {
+  const num1 = parseFloat(firstValue);
+  const num2 = parseFloat(displayValue);
+  if (operator === "+") {
+    setDisplayValue((num1 + num2).toString());
+  } else if (operator === "-") {
+    setDisplayValue((num1 - num2).toString());
+  } else if (operator === "*") {
+    setDisplayValue((num1 * num2).toString());
+  } else if (operator === "/") {
+    setDisplayValue((num1 / num2).toString());
+  }else if (operator === "%") {
+    setDisplayValue((num1 % num2).toString());
+  }
+  setFirstValue("");
+  setOperator("");
+}
+const handleClear = () => {
+  setFirstValue("");
+  setDisplayValue("0");
+  setOperator("");
+}
+const handleBackspace = () => {
+  if (displayValue.length === 1) {
+    setDisplayValue("0");
+  } else {
+    setDisplayValue(displayValue.slice(0, -1));
+  }
+}
     return (
         <View style={styles.container}>
             <View style={styles.display}>
+              <Text style={{fontSize:30,fontWeight:'semibold'}}>{firstValue + operator}</Text>
               <Text style={{fontSize:70,fontWeight:'semibold'}}>{displayValue}</Text>
             </View>
             <View style={styles.Keypad}>
-               <BButton title='C' type='top'/>
-              <BButton title='«' type='top'/>
-<BButton title='%' type='top'/>
-<BButton title='+' type='right'/>
-<BButton title='7' type='number' onPress={() => handleNumberInput(7)}/>
-<BButton title='8' type='number' onPress={() => handleNumberInput(8)}/>
-<BButton title='9' type='number' onPress={() => handleNumberInput(9)}/>
-<BButton title='x' type='right'/>
+               <BButton title='C' type='top' onPress={handleClear}/>
+              <BButton title='«' type='top' onPress={handleBackspace}/>
+<BButton title='%' type='top' onPress={() => handleOperatorInput('%')}/>
+<BButton title='/' type='right' onPress={() => handleOperatorInput('/')}/>
+<BButton title='7' type='number' onPress={() => handleNumberInput('7')}/>
+<BButton title='8' type='number' onPress={() => handleNumberInput('8')}/>
+<BButton title='9' type='number' onPress={() => handleNumberInput('9')}/>
+<BButton title='x' type='right' onPress={() => handleOperatorInput('*')}/>
 <BButton title='6' type='number' onPress={() => handleNumberInput('6')}/>
 <BButton title='5' type='number' onPress={() => handleNumberInput('5')}/>
 <BButton title='4' type='number' onPress={() => handleNumberInput('4')}/>
-<BButton title='-' type='right'/>
+<BButton title='-' type='right' onPress={() => handleOperatorInput('-')}/>
 <BButton title='1' type='number' onPress={() => handleNumberInput('1')}/>
 <BButton title='2' type='number' onPress={() => handleNumberInput('2')}/>
 <BButton title='3' type='number' onPress={() => handleNumberInput('3')}/>
-<BButton title='+' type='right'/>
+<BButton title='+' type='right' onPress={() => handleOperatorInput('+')}/>
 <BButton title='0' type='number' onPress={() => handleNumberInput('0')}/> 
 <BButton title='00' type='number' onPress={() => handleNumberInput('00')}/>
 <BButton title='.' type='number' onPress={() => handleNumberInput('.')}/>
-<BButton title='=' type='right'/>
+<BButton title='=' type='right' onPress={handleEquals} />
             </View>
         </View>
     );
